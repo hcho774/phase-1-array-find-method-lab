4608c625c04211e804df9fcbf0c02e1197d003c9
Hyun Sung Cho <hcho774@gmail.com>
2022-04-06 14:28:27 -0400

    commit tree e148c538df7beb02304e2390177a3c0cd635e464

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function findWin(item){
+    return item.result === "W";
+} 
+
+
+function superbowlWin(arr) {
+    if(arr.find(findWin)){
+        return arr.find(findWin).year;
+    }
+}
+
